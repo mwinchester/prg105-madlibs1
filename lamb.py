b1cc54cb8272407792903051a1d5c354b2bd30f2
Meri Winchester <mwinchester@users.noreply.github.com>
2016-08-16 19:50:11 -0500

    Add files via upload

---
+++ lamb.py
@@ -0,0 +1,31 @@
+# Variables
+person = "Steve Jobs"
+pet = "lizard"
+color = "green"
+example = "spinach"
+place = "theatre"
+group = "singers"
+person2 = "Doctor Who"
+adverb = "quietly"
+action = "sings"
+
+# poem
+print(person + " had a little " + pet + ",")
+print("His fleece was " + color + " as " + example + ",")
+print("And everywhere that " + person + " went,")
+print("The " + pet + " was sure to go.\n")
+
+print("He followed her to " + place + " one day,")
+print("Which was against the rule,")
+print("It made the " + group + " laugh and play")
+print("To see a " + pet + " at " + place + ".\n")
+
+print("And so the " + person2 + " turned it out,")
+print("But still it lingered near,")
+print("And waited " + adverb + " about,")
+print("Till " + person + " did appear.\n")
+
+print("Why does the " + pet + " love " + person + " so?")
+print("The eager " + group + " cry.")
+print("Why, " + person +" " + action + " the " + pet + ", you know,")
+print("The " + person2 + " did reply.")
